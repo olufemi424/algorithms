db31a7a9c93efc6fcaab14e9fffbbe4b73866a7b
Olufemi Afolabi <olu@Olufemis-MacBook-Pro.local>
2018-12-07 17:54:34 -0600

    update

---
+++ findMax.js
@@ -0,0 +1,16 @@
+// write a function that takes in an array of numbers and return the largest num
+
+// wtite a fucntion that takes in an array of numbers and return the smallest num
+
+function findMax(arrNums) {
+  let max = 0;
+  for (num of arrNums) {
+    if (num >= max) {
+      max = num;
+    }
+  }
+
+  return max;
+}
+
+console.log(findMax([77, -5, 2, 89, 44, 37]));

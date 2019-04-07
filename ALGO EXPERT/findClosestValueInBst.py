bc882faa344a29e6c890cfc577b2734a5d849691
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-04-07 12:38:08 -0500

    depth first search tree traversing

--- ALGO EXPERT/findClosestValueInBst.py
+++ ALGO EXPERT/findClosestValueInBst.py
@@ -28,19 +28,19 @@
 # Average: O(Log(n)) | O(1)
 # worst: O(n) time | O(1) space
 
-def findClosestValueInBst(tree, target):
-    return findClosestValueInBstHelper(tree, target, float('inf'))
+# def findClosestValueInBst(tree, target):
+#     return findClosestValueInBstHelper(tree, target, float('inf'))
 
 
-def findClosestValueInBstHelper(tree, target, closest):
-    currentNode = tree
-    while currentNode is not None:
-        if abs(target - closest) > abs(target - currentNode.value):
-            closest = currentNode.value
-        if target < currentNode.value:
-            currentNode = currentNode.left
-        elif target > currentNode.value:
-            currentNode = currentNode.right
-        else:
-            break
-    return closest
+# def findClosestValueInBstHelper(tree, target, closest):
+#     currentNode = tree
+#     while currentNode is not None:
+#         if abs(target - closest) > abs(target - currentNode.value):
+#             closest = currentNode.value
+#         if target < currentNode.value:
+#             currentNode = currentNode.left
+#         elif target > currentNode.value:
+#             currentNode = currentNode.right
+#         else:
+#             break
+#     return closest

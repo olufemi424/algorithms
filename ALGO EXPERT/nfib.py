5174bfc15e019d942f28513de1302703b01d2b90
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-04-11 10:46:43 -0500

    nth fib funtion

---
+++ ALGO EXPERT/nfib.py
@@ -0,0 +1,8 @@
+# Nth Fibonacci
+
+# The fibonacci sequence is defined as follows: the first number of the sequence is 0, the second number is 1, and the nth number is the sum of the(n-1)th and (n-2)th numbers. Write a function that takes in an integer n and returns the nth Fibobacci number
+
+
+# sample input: 6
+
+# sample output: 5(0, 1, 1, 2, 3, 5)

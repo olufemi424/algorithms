e7f6a20d9e48a1ea95bb45c96343266d9b22ea06
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-04-19 16:23:44 -0500

    Binary Search

---
+++ ALGO EXPERT/binarySearch.py
@@ -0,0 +1,7 @@
+# Binary Search
+
+# Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to find it the target number is contained in the array and should return its index if it is, otherwise -1
+
+
+# sample input: [0,1,21,33,45,45,61,71,72,73], 33
+# Sample output: 3

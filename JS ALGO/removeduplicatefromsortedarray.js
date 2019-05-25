5e803c5b7a50b3a7dd724597ed7c5daeacb75b27
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-05-24 21:45:11 -0500

    remove element

--- JS ALGO/removeduplicatefromsortedarray.js
+++ JS ALGO/removeduplicatefromsortedarray.js
@@ -1,6 +1,9 @@
 const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
 const removeDuplicates = function(nums) {
+  if (nums.length === 0) return 0;
+  if (nums === null) return 0;
+
   let j = 0;
 
   for (let i = 1; i < nums.length; i++) {

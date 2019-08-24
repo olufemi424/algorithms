4d670199504274693dbe36e9e241cadb9d2740a2
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-08-24 13:39:49 -0400

    selection sort comment

--- ALGO EXPERT/selectionSort.js
+++ ALGO EXPERT/selectionSort.js
@@ -1,4 +1,12 @@
 
+//Finding the smallest with insertion sort, starting from the index 0, which is the first item on the list, set it as the current index
+
+//while the current index, which is index 0 is less than the index of the last element in the array. Keep checking the rest of the items for possible item smaller than the items at the current index. 
+
+//if found, swap the current smallest with the new smallest
+
+//go back into the while look to check to the next current number, not considering the last swapped number
+
 function selectionSort(arr){
    let currIndex = 0
    while (currIndex < arr.length){
